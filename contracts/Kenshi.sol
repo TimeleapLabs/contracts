efbc7890fba3b8decbb495cfcd03b5d3bea92d69
Pouya <pouya.eghbali@yandex.com>
2023-05-07 21:21:42 +0200

    ERC1363 -> ERC20

--- contracts/Kenshi.sol
+++ contracts/Kenshi.sol
@@ -2,16 +2,11 @@
 pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
-import "@openzeppelin/contracts/interfaces/IERC165.sol";
-import "@openzeppelin/contracts/interfaces/IERC1363.sol";
-import "@openzeppelin/contracts/interfaces/IERC1363Receiver.sol";
-import "@openzeppelin/contracts/interfaces/IERC1363Spender.sol";
-
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Kenshi is Context, IERC165, IERC1363, Ownable {
+contract Kenshi is Context, IERC20, Ownable {
     using Address for address;
     /* ERC20 related */
 
@@ -195,8 +190,6 @@ contract Kenshi is Context, IERC165, IERC1363, Ownable {
         return true;
     }
 
-    event Reflect(uint256 amount);
-
     /**
      * @dev Moves tokens `amount` from `sender` to `recipient`.
      *
@@ -204,7 +197,6 @@ contract Kenshi is Context, IERC165, IERC1363, Ownable {
      * e.g. implement automatic token fees, slashing mechanisms, etc.
      *
      * Emits a {Transfer} event.
-     * Emits a {Reflect} event.
      *
      * Requirements:
      *
@@ -253,177 +245,6 @@ contract Kenshi is Context, IERC165, IERC1363, Ownable {
         emit Approval(addr, spender, amount);
     }
 
-    /* ERC165 methods */
-
-    /**
-     * @dev See {IERC165-supportsInterface}.
-     */
-    function supportsInterface(
-        bytes4 interfaceId
-    ) external pure returns (bool) {
-        return
-            interfaceId == type(IERC1363).interfaceId ||
-            interfaceId == type(IERC20).interfaceId ||
-            interfaceId == type(IERC165).interfaceId;
-    }
-
-    /* ERC1363 methods */
-
-    /**
-     * @dev Transfer tokens to a specified address and then execute a callback on recipient.
-     * @param recipient The address to transfer to.
-     * @param amount The amount to be transferred.
-     * @return A boolean that indicates if the operation was successful.
-     */
-    function transferAndCall(
-        address recipient,
-        uint256 amount
-    ) external returns (bool) {
-        return transferAndCall(recipient, amount, "");
-    }
-
-    /**
-     * @dev Transfer tokens to a specified address and then execute a callback on recipient.
-     * @param recipient The address to transfer to
-     * @param amount The amount to be transferred
-     * @param data Additional data with no specified format
-     * @return A boolean that indicates if the operation was successful.
-     */
-    function transferAndCall(
-        address recipient,
-        uint256 amount,
-        bytes memory data
-    ) public returns (bool) {
-        transfer(recipient, amount);
-        require(
-            _checkAndCallTransfer(_msgSender(), recipient, amount, data),
-            "ERC1363: _checkAndCallTransfer reverts"
-        );
-        return true;
-    }
-
-    /**
-     * @dev Transfer tokens from one address to another and then execute a callback on recipient.
-     * @param sender The address which you want to send tokens from
-     * @param recipient The address which you want to transfer to
-     * @param amount The amount of tokens to be transferred
-     * @return A boolean that indicates if the operation was successful.
-     */
-    function transferFromAndCall(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external returns (bool) {
-        return transferFromAndCall(sender, recipient, amount, "");
-    }
-
-    /**
-     * @dev Transfer tokens from one address to another and then execute a callback on recipient.
-     * @param sender The address which you want to send tokens from
-     * @param recipient The address which you want to transfer to
-     * @param amount The amount of tokens to be transferred
-     * @param data Additional data with no specified format
-     * @return A boolean that indicates if the operation was successful.
-     */
-    function transferFromAndCall(
-        address sender,
-        address recipient,
-        uint256 amount,
-        bytes memory data
-    ) public returns (bool) {
-        transferFrom(sender, recipient, amount);
-        require(
-            _checkAndCallTransfer(sender, recipient, amount, data),
-            "ERC1363: _checkAndCallTransfer reverts"
-        );
-        return true;
-    }
-
-    /**
-     * @dev Approve spender to transfer tokens and then execute a callback on recipient.
-     * @param spender The address allowed to transfer to
-     * @param amount The amount allowed to be transferred
-     * @return A boolean that indicates if the operation was successful.
-     */
-    function approveAndCall(
-        address spender,
-        uint256 amount
-    ) external returns (bool) {
-        return approveAndCall(spender, amount, "");
-    }
-
-    /**
-     * @dev Approve spender to transfer tokens and then execute a callback on recipient.
-     * @param spender The address allowed to transfer to.
-     * @param amount The amount allowed to be transferred.
-     * @param data Additional data with no specified format.
-     * @return A boolean that indicates if the operation was successful.
-     */
-    function approveAndCall(
-        address spender,
-        uint256 amount,
-        bytes memory data
-    ) public returns (bool) {
-        approve(spender, amount);
-        require(
-            _checkAndCallApprove(spender, amount, data),
-            "ERC1363: _checkAndCallApprove reverts"
-        );
-        return true;
-    }
-
-    /**
-     * @dev Internal function to invoke `onTransferReceived` on a target address
-     *  The call is not executed if the target address is not a contract
-     * @param sender address Representing the previous owner of the given token value
-     * @param recipient address Target address that will receive the tokens
-     * @param amount uint256 The amount mount of tokens to be transferred
-     * @param data bytes Optional data to send along with the call
-     * @return whether the call correctly returned the expected magic value
-     */
-    function _checkAndCallTransfer(
-        address sender,
-        address recipient,
-        uint256 amount,
-        bytes memory data
-    ) internal returns (bool) {
-        if (!recipient.isContract()) {
-            return false;
-        }
-        bytes4 retval = IERC1363Receiver(recipient).onTransferReceived(
-            _msgSender(),
-            sender,
-            amount,
-            data
-        );
-        return (retval ==
-            IERC1363Receiver(recipient).onTransferReceived.selector);
-    }
-
-    /**
-     * @dev Internal function to invoke `onApprovalReceived` on a target address
-     *  The call is not executed if the target address is not a contract
-     * @param spender address The address which will spend the funds
-     * @param amount uint256 The amount of tokens to be spent
-     * @param data bytes Optional data to send along with the call
-     * @return whether the call correctly returned the expected magic value
-     */
-    function _checkAndCallApprove(
-        address spender,
-        uint256 amount,
-        bytes memory data
-    ) internal returns (bool) {
-        if (!spender.isContract()) {
-            return false;
-        }
-        bytes4 retval = IERC1363Spender(spender).onApprovalReceived(
-            _msgSender(),
-            amount,
-            data
-        );
-        return (retval == IERC1363Spender(spender).onApprovalReceived.selector);
-    }
-
     /**
      * @dev Sends `amount` of ERC20 `token` from contract address to `recipient`
      *
@@ -434,7 +255,6 @@ contract Kenshi is Context, IERC165, IERC1363, Ownable {
         address recipient,
         uint256 amount
     ) external onlyOwner returns (bool) {
-        require(token != address(this), "Kenshi: cannot recover KNS");
         return IERC20(token).transfer(recipient, amount);
     }
 }
